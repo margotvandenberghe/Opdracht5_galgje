861a27c7ad8694fc8be1290e0a5baf33fe376a7d
student <student@gebruikers-mac-mini-2.local>
2018-10-18 10:04:45 +0200

    Initial Commit

---
+++ Opdracht5_galgje/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Opdracht5_galgje
+//
+//  Created by student on 18/10/2018.
+//  Copyright © 2018 Vives. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
